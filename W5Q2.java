71f654c12900d5969960ebefa88efd7365071025
Rajveer Singh Bais <rs1127@srmist.edu.in>
2026-03-30 14:30:20 +0530

    Added

---
+++ W5Q2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class W5Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+
+        // Taking input
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+
+        System.out.println("\nNumber Analysis:");
+
+        // Checking each number
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 0) {
+                if (numbers[i] % 2 == 0) {
+                    System.out.println(numbers[i] + " is Positive and Even.");
+                } else {
+                    System.out.println(numbers[i] + " is Positive and Odd.");
+                }
+            } else if (numbers[i] < 0) {
+                System.out.println(numbers[i] + " is Negative.");
+            } else {
+                System.out.println(numbers[i] + " is Zero.");
+            }
+        }
+
+        // Comparing first and last element
+        System.out.println("\nComparison of First and Last Element:");
+        if (numbers[0] == numbers[numbers.length - 1]) {
+            System.out.println("First and last elements are equal.");
+        } else if (numbers[0] > numbers[numbers.length - 1]) {
+            System.out.println("First element is greater than last element.");
+        } else {
+            System.out.println("First element is less than last element.");
+        }
+
+        sc.close();
+    }
+}
